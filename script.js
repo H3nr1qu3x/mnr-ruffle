a3756a42db2af7e38bc67a40bdd7235b336182a2
Henrique <104533111+cotamilhas@users.noreply.github.com>
2024-05-13 10:11:04 +0100

    config script

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-var swfobject = {};
+window.RufflePlayer = window.RufflePlayer || {};
+window.RufflePlayer.config = {
+    // Options affecting the whole page
+    "publicPath": undefined,
+    "polyfills": true,
 
-swfobject.embedSWF = function(url, cont, width, height){
-    var ruffle = window.RufflePlayer.newest(),
-        player = Object.assign(document.getElementById(cont).appendChild(ruffle.createPlayer()), {
-            width: width,
-            height: height,
-            style: 'width: ' + width + 'px; height: ' + height + 'px',
-        });
-
-    player.load({ url: url });
-}
-
+    // Options affecting files only
+    "allowScriptAccess": false, // Polyfill elements have a different default value, see the allowScriptAccess section
+    "autoplay": "auto",
+    "unmuteOverlay": "visible",
+    "backgroundColor": null,
+    "wmode": "window",
+    "letterbox": "fullscreen",
+    "warnOnUnsupportedContent": true,
+    "contextMenu": "on",
+    "showSwfDownload": false,
+    "upgradeToHttps": window.location.protocol === "https:",
+    "maxExecutionDuration": 15,
+    "logLevel": "error",
+    "base": null,
+    "menu": true,
+    "salign": "",
+    "forceAlign": false,
+    "scale": "showAll",
+    "forceScale": false,
+    "frameRate": null,
+    "quality": "high",
+    "splashScreen": true,
+    "preferredRenderer": null,
+    "openUrlMode": "allow",
+    "allowNetworking": "all",
+    "favorFlash": true,
+    "socketProxy": [],
+    "fontSources": [],
+    "defaultFonts": {},
+    "credentialAllowList": [],
+    "playerRuntime": "flashPlayer",
+    "allowFullscreen": false
+};
