19cd7d11937f7c188a4f10bd7d79970e3058bf0d
Henrique <104533111+cotamilhas@users.noreply.github.com>
2024-05-13 15:11:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -24,7 +24,7 @@ window.RufflePlayer.config = {
     "scale": "showAll",
     "forceScale": false,
     "frameRate": 60,
-    "quality": "high",
+    "quality": "best",
     "splashScreen": false,
     "preferredRenderer": null,
     "openUrlMode": "allow",
